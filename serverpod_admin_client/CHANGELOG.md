4d1071a82d49b1abb2cbc485e495396a9dff8135
AdamMusa <adammusa2222@gmail.com>
2025-12-16 01:13:32 -0500

    replace the images

--- serverpod_admin_client/CHANGELOG.md
+++ serverpod_admin_client/CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## 1.0.4
+## 1.0.5
 
 - Initial version
 - Client-side protocol definitions for admin module
--- serverpod_wire_runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Runner script for serverpod_wire that uses dart run for reliable execution
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR"
-dart run serverpod_wire/bin/serverpod_wire.dart "$@"
-
